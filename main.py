60e49aff6846cecbbcb08177af73d4c066600a17
dusehdrb <psj101033@gmail.com>
2025-06-10 14:38:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.title('나의 첫 app')
 st.write('hello')
+name=st.text_input('이름 입력')
+if name:
+  if name==('mc소울'):
+    st.success('반갑습니다.mc소울님!')
+else:
+  st.warning('누구세요')
